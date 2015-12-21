0fe6d16429ee1a728cf3e980a13258f7a5afb55c
ctkochan22 <ctkochan22@gmail.com>
2015-12-21 13:31:45 -0800

    Adds readme

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -17,19 +17,35 @@ class PackagesController < ApplicationController
     # p @response
   end
 
-  def test
-    p "TEEEEEESSSST"
-  end
+  # def hashify(package)
+  #   if package.kind_of?(Array)
+  #     #Run if Array
+
+  #   elsif package.kind_of?(Hash)
+  #     #Run if Hash
+  #     # length = package.length()
+  #     keys = package.keys
+  #     if keys.length == 1
+
+
+  #   else
+  #     #Run if anything else
+  #   end
+  # end
 end
 
 
-=begin
-Stops for a given route:
-http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfi
-g&a=sf-muni&r=N
 
-Get Routes
-http://services.my511.org/Transit2.0/GetRoutesForAgencies.aspx?token=d7addd58-ca1e-4395-96f6-55440646c3ae&agencyNames=SF-MUNI
+# Stops for a given route:
+# http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfi
+# g&a=sf-muni&r=N
+#http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=N
 
+# Get Routes
+# http://services.my511.org/Transit2.0/GetRoutesForAgencies.aspx?token=d7addd58-ca1e-4395-96f6-55440646c3ae&agencyNames=SF-MUNI
 
-=end
+# {"A" => {"1"=> "One", "2 => "Two"},
+# "B" => "bee",
+# "C" => ['a', 'b', 'c']
+# }
+
