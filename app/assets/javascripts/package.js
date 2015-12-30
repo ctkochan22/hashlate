abcaa63b6b610be87c696ff849162814cb713ab1
ctkochan22 <ctkochan22@gmail.com>
2015-12-29 16:57:41 -0800

    Fixes the extra linebreak for commas

--- app/assets/javascripts/package.js
+++ app/assets/javascripts/package.js
@@ -15,14 +15,32 @@ $(document).ready(function(){
     });//done
   }); //'$form'
 
-  $('#api-container').on('click', 'p', function(event){
-    event.preventDefault();
-    console.log(this);
-  })
+  click_for_path();
 
+  $('#close-btn').on('click', function(event){
+    event.preventDefault();
+    $('#path-display').empty();
+    $('#path-container').hide();
+  })
 });//$document ready
 
 
+function click_for_path() {
+  $('#api-container').on('click', 'p', function(event){
+    event.preventDefault();
+    var path = $(this).attr("data-path");
+    var path_html = "<p id='path-show'>" + path + "</p>";
+    console.log(path_html);
+
+    $('#path-display').html(path_html);
+    $('#path-container').show();
+  });
+};
+
+
+
+
+// ### UN-USED FUNCTIONS ##
 // #hashify -- RECURSIVE -- one package
 function hashify(obj_package, base_path){
   var full_html = "";
@@ -87,97 +105,3 @@ function hashify(obj_package, base_path){
   };
 }; //
 
-
-// #hashify -- recursive -- multiple packages **DOESNT FIX ***
-// function hashify_append(obj_package, base_path, container){
-//   var full_html ="";
-//   $('#api-container').empty();
-
-//   // ARRAY
-//   if (Array.isArray(obj_package)) {
-//     console.log("Caught Array");
-
-//     var pre_array_html ="";
-//     pre_array_html = "<p data-path='" + base_path + "'>[</p>";
-//     $(container).append(pre_array_html);
-
-//     for (var i = 0; i < obj_package.length; i++) {
-//       var new_base_path = base_path + "[" + i.toString() + "]";
-//       var new_html_item = hashify(obj_package[i], new_base_path);
-//       var full_html_item;
-
-//       if (i === (obj_package.length - 1)) {
-//         full_html_item = "<p data-path='" + new_base_path + "'>" + new_html_item + "</p>";
-//       } else {
-//         full_html_item = "<p data-path='" + new_base_path + "'>" + new_html_item + ",</p>";
-//       };
-//       // full_html += full_html_item;
-//       $(container).append(full_html_item);
-//     }; //for loop
-
-//     pre_array_html = "<p data-path='" + base_path + "'>]</p>";
-
-//     $(container).append(pre_array_html);
-
-//     return;
-
-//   //OBJECT
-//   } else if (typeof obj_package === 'object') {
-//     console.log("Caught Object");
-
-//     var pre_object_html;
-//     pre_object_html = "<p data-path='" + base_path + "'>{</p>";
-//     $(container).append(pre_object_html);
-
-//     var object_html = "";
-//     for (var key in obj_package){
-//       // ** need to do something with key
-//       var new_base_path = base_path + "[" + key + "]";
-//       var new_html_item = hashify(obj_package[key], new_base_path);
-//       var full_html_item = "<p data-path='" + new_base_path + "'>" + key + ": " + new_html_item + ",</p>";
-//       object_html += full_html_item;
-//     };
-//     object_html = object_html.slice(0, -5) + object_html.slice(-4);
-
-//     $(container).append(object_html);
-//     pre_object_html = "<p data-path='" + base_path + "'>}</p>";
-//     $(container).append(pre_object_html);
-
-//     return;
-
-//   //SINGLE ELEMENT
-//   } else {
-//     console.log("Caught Single Element");
-
-//     if (typeof obj_package === "number") {
-//       $(container).append(obj_package.toString());
-//     } else if (typeof obj_package === "string") {
-//       $(container).append(obj_package);
-//     } else if (typeof obj_package === "boolean") {
-//       $(container).append(obj_package.toString());
-//     } else if (typeof obj_package === "undefined") {
-//       return "UNDEFINED";
-//     } else {
-//       //ERROR CASE
-//       return "ERROR: Fatal Failure";
-//     };
-
-//   };
-// }; //
-
-
-
-
-//Function turns link into string
-function xml_to_string(input){
-    $.ajax({
-      type: 'GET',
-      url: input
-    }).done(function(response){
-      console.log(response);
-      var xmlText = new XMLSerializer().serializeToString(response);
-      var xmlTextNode = document.createTextNode(xmlText);
-      console.log(xmlTextNode);
-      $('#api-container').html(xmlTextNode);
-    });
-}
--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -7,59 +7,67 @@ class PackagesController < ApplicationController
   def hash
     url = params[:url]
     response = HTTParty.get(url).parsed_response
-    @html_package = hashify(response, "")
+    @html_package = hashify(response, "", 5)
     ap @html_package
     render "packages/_package", layout: false
   end
 
 
-
-
-
-def hashify(obj_package, base_path)
+def hashify(obj_package, base_path, indent)
   full_html = ""
+  new_indent = indent * 2
+  indent_html = "text-indent:#{indent.to_s}px;"
+  new_indent_html = "text-indent:#{new_indent.to_s}px;"
 
   if obj_package.kind_of?(Array)
     puts("Caught Array")
 
-    full_html += "<p data-path=#{base_path}>[</p>"
+    full_html += "<p data-path=#{base_path} style=#{new_indent_html}>[</p>"
     obj_package.each_with_index do |element, index|
       new_base_path = base_path + "[#{index.to_s}]"
-      new_html_item = hashify(obj_package[index], new_base_path)
+      new_html_item = hashify(obj_package[index], new_base_path, new_indent)
       if index == (obj_package.length - 1)
-        full_html_item = "<p data-path=#{new_base_path}>#{new_html_item}</p>"
+        full_html_item = "<p data-path=#{new_base_path} style=#{new_indent_html}>#{new_html_item}</p>"
       else
-        full_html_item = "<p data-path=#{new_base_path}>#{new_html_item},</p>"
+        if new_html_item[0..1] == "<p"
+          full_html_item = new_html_item[0...-4] + "," + new_html_item[-4..-1]
+        else
+          full_html_item = "<p data-path=#{new_base_path} style=#{new_indent_html}>#{new_html_item},</p>"
+        end
       end
       full_html += full_html_item
     end #each loop
-    full_html += "<p data-path=#{base_path}>]</p>"
+    full_html += "<p data-path=#{base_path} style=#{new_indent_html}>]</p>"
     return full_html
 
   elsif obj_package.kind_of?(Hash)
     puts("Caught Object")
     object_html = ""
 
-    full_html += "<p data-path=#{base_path}>{</p>"
+    full_html += "<p data-path=#{base_path} style=#{new_indent_html}>{</p>"
 
     obj_package.each do |key, value|
       new_base_path = base_path + "['#{key}']"
-      new_html_item = hashify(obj_package[key], new_base_path)
-      full_html_item = "<p data-path=#{new_base_path}>#{key}: #{new_html_item},</p>"
+      new_html_item = hashify(obj_package[key], new_base_path, new_indent)
+      if new_html_item[0..1] == "<p"
+        full_html_item = new_html_item[0...-4] + "," + new_html_item[-4..-1]
+      else
+        full_html_item = "<p data-path=#{new_base_path} style=#{new_indent_html}>#{key}: #{new_html_item},</p>"
+      end
       object_html += full_html_item
     end
 
     object_html = object_html[0... -5] + object_html[-4..-1]
     full_html += object_html
-    full_html += "<p data-path=#{base_path}>}</p>"
+    full_html += "<p data-path=#{base_path} style=#{new_indent_html}>}</p>"
     return full_html
   else
     puts("Caught Single Element")
 
     if obj_package.is_a? Numeric
-      return obj_package.to_s
+      return "'" + obj_package.to_s + "'"
     elsif obj_package.is_a? String
-      return obj_package
+      return "'" + obj_package + "'"
     elsif !!obj_package == obj_package
       return obj_package.to_s
     elsif obj_package == nil
